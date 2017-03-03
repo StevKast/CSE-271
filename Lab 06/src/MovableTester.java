9cb44fddea365ec89bc3107f7649728ab8b08f12
Steven Kast <steven.m.kast@gmail.com>
2017-03-03 12:58:05 -0500

    Almost done with tester

--- Lab 06/src/MovableTester.java
+++ Lab 06/src/MovableTester.java
@@ -1,15 +1,39 @@
 
 public class MovableTester {
-	
+
 	public static void main(String[] args){
-		Movable m1 = new MovablePoint(5, 6, 10,12); // upcast
+		Movable m1 = new MovablePoint(5, 6, 10,12);
 		System.out.println(m1);
 		m1.moveLeft();
 		System.out.println(m1);
-		Movable m2 = new MovableCircle(2, 1, 2, 20,50); // upcast. Constructor takes in 4 point values and
-														//radius
+		
+		Movable m2 = new MovableCircle(2, 1, 2, 20,50);
 		System.out.println(m2);
 		m2.moveRight();
 		System.out.println(m2);
+		
+		Movable[] mArray = {m1, m2};
+		chaChaSlide(mArray);
+		
+		System.out.println("\n" + m1);
+		System.out.println(m2);
+	}
+
+	public static void chaChaSlide(Movable[] movable){
+		for(Movable point: movable){
+			point.moveLeft();
+			point.moveRight();
+			point.moveDown();
+			point.moveUp();
+			point.moveLeft();
+			point.moveDown();
+			point.moveUp();
+			point.moveRight();
+			point.moveLeft();
+			point.moveDown();
+			point.moveUp();
+			point.moveUp();
+
+		}
 	}
 }
