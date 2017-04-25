1b8d163882071ccc62d99ff4b48bb180e9c9e3ed
Steven Kast <steven.m.kast@gmail.com>
2017-04-25 17:25:35 -0400

    Adding grid for board

--- Project 2/src/edu/miamioh/kastsm/MainFrame.java
+++ Project 2/src/edu/miamioh/kastsm/MainFrame.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.Color;
 
 public class MainFrame extends JFrame {
 
@@ -35,8 +37,13 @@ public class MainFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel boardGrid = new JPanel();
+		boardGrid.setBackground(Color.WHITE);
+		contentPane.add(boardGrid, BorderLayout.EAST);
+		boardGrid.setLayout(new GridLayout(8, 8, 0, 0));
 	}
 
 }
