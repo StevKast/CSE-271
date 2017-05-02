0fccd19917f377cd3e9721a381ae683fe111567e
Steven Kast <steven.m.kast@gmail.com>
2017-05-02 13:22:26 -0400

    Starting to use absolute layout

--- Project 2/src/edu/miamioh/kastsm/MainFrame.java
+++ Project 2/src/edu/miamioh/kastsm/MainFrame.java
@@ -20,6 +20,18 @@ import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame {
 
@@ -37,6 +49,7 @@ public class MainFrame extends JFrame {
 				try {
 					MainFrame frame = new MainFrame();
 					frame.setVisible(true);
+					frame.setSize(850, 500);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -48,60 +61,70 @@ public class MainFrame extends JFrame {
 	 * Create the main frame.
 	 */
 	public MainFrame() {
-		setTitle("n-Queens Problem\r\n");
+		setBackground(Color.WHITE);
+		setResizable(false);
+		setTitle("N-Queens Problem\r\n");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 805, 500);
+		setBounds(100, 100, 800, 500);
+		//setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-				
-				JPanel welcomePanel = new JPanel();
-				contentPane.add(welcomePanel);
-				welcomePanel.setLayout(new GridLayout(2, 1, 0, 0));
-				
-				JLabel welcomeTitle = new JLabel("N-Queens Problem Tool:");
-				welcomePanel.add(welcomeTitle);
-				
-				JLabel welcomeParagraph = new JLabel("g");
-				welcomePanel.add(welcomeParagraph);
+				contentPane.setLayout(null);
 		
-				JPanel countPanel = new JPanel();
-				contentPane.add(countPanel);
-				
-						//Label
-						JLabel lblNumberOfQueens = new JLabel("Current Number of Placed Queens: ");
-						countPanel.add(lblNumberOfQueens);
-						
-								//TextField that displays current number of queens placed
-								currentQueens = new JTextField();
-								currentQueens.setEditable(false);
-								currentQueens.setText("0");
-								countPanel.add(currentQueens);
-								currentQueens.setColumns(2);
+				JLabel welcomeTitle = new JLabel("N-Queens Problem Tool");
+				welcomeTitle.setBounds(5, 5, 784, 27);
+				welcomeTitle.setHorizontalAlignment(SwingConstants.CENTER);
+				contentPane.add(welcomeTitle);
+				welcomeTitle.setFont(new Font("Tahoma", Font.PLAIN, 22));
 
 		JPanel boardGrid = new JPanel();
+		boardGrid.setBounds(new Rectangle(5, 32, 400, 400));
+		boardGrid.setPreferredSize(new Dimension(400, 400));
 		boardGrid.setBorder(new LineBorder(new Color(0, 0, 0), 3));
 		boardGrid.setBackground(Color.WHITE);
-		boardGrid.setBounds(380, 47, 400, 400);
 		contentPane.add(boardGrid);
 		boardGrid.setLayout(new GridLayout(8, 8, 0, 0));
+
+		//Panel to display the current amount of queens placed
+		JPanel countPanel = new JPanel();
+		countPanel.setLocation(32, 431);
+		contentPane.add(countPanel);
+		countPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JLabel lblNumberOfQueens = new JLabel("Current Number of Placed Queens: ");
+		countPanel.add(lblNumberOfQueens);
+		countPanel.setSize(350, 30);
+		currentQueens = new JTextField();
+		currentQueens.setEditable(false);
+		currentQueens.setText("0");
+		countPanel.add(currentQueens);
+		currentQueens.setColumns(2);
+		
+		JButton checkSolution = new JButton("Check Solution");
+		checkSolution.setBounds(534, 372, 138, 60);
+		contentPane.add(checkSolution);
+		
+		JTextArea instructionTextArea = new JTextArea();
+		instructionTextArea.setEditable(false);
+		instructionTextArea.setBounds(425, 40, 300, 300);
+		contentPane.add(instructionTextArea);
 
 		gridArray = new JToggleButton[8][8];
 
 		ImageIcon queenIcon = new ImageIcon(MainFrame.class.getResource("Freddie-Mercury.jpg"));
-		
+
 		ActionListener buttonListener = new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent evt) {
-				// Finds which button the event came from
-				JToggleButton selectedButton = (JToggleButton) evt.getSource();
+			public void actionPerformed(ActionEvent event) {
+				//Creates a temp button that represents the clicked one.
+				JToggleButton selectedButton = (JToggleButton) event.getSource();
+				
 				for (int row = 0; row < gridArray.length; row++) {
 					for (int col = 0; col < gridArray[row].length; col++) {
 						// Just helpful console output---User won't see it
 						if (gridArray[row][col] == selectedButton) {
 							if (selectedButton.isSelected()) {
-								//selectedButton.setIcon(queenIcon);
+								selectedButton.setIcon(queenIcon);
 								System.out.printf("Selected row and column: (%d, %d)%n", row + 1, col + 1);
 								queens++;
 								currentQueens.setText(Integer.toString(queens));
@@ -116,6 +139,7 @@ public class MainFrame extends JFrame {
 				}
 			}
 		};
+		
 
 		for (int row = 0; row < gridArray.length; row++) {
 			for (int col = 0; col < gridArray[row].length; col++) {
@@ -126,12 +150,10 @@ public class MainFrame extends JFrame {
 					gridArray[row][col] = new JToggleButton();
 					gridArray[row][col].setBackground(Color.WHITE);
 				}
-				gridArray[row][col].setSize(50, 50);
 				gridArray[row][col].addActionListener(buttonListener);
 				boardGrid.add(gridArray[row][col]);
 			}
 		}
 
 	}
-
 }
