edfc5317d5617eaba6f5dc3ca7f013709b8a41ab
steve <steve@172.25.53.255>
2017-05-02 16:47:03 -0400

    Row and Column checking done.

--- Project 2/src/edu/miamioh/kastsm/MainFrame.java
+++ Project 2/src/edu/miamioh/kastsm/MainFrame.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
@@ -32,6 +33,7 @@ import javax.swing.SwingConstants;
 import java.awt.SystemColor;
 import javax.swing.JTextPane;
 import javax.swing.JTextArea;
+import javax.swing.JScrollBar;
 
 public class MainFrame extends JFrame {
 
@@ -66,17 +68,16 @@ public class MainFrame extends JFrame {
 		setTitle("N-Queens Problem\r\n");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 500);
-		//setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-				contentPane.setLayout(null);
-		
-				JLabel welcomeTitle = new JLabel("N-Queens Problem Tool");
-				welcomeTitle.setBounds(5, 5, 784, 27);
-				welcomeTitle.setHorizontalAlignment(SwingConstants.CENTER);
-				contentPane.add(welcomeTitle);
-				welcomeTitle.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		contentPane.setLayout(null);
+
+		JLabel welcomeTitle = new JLabel("N-Queens Problem Tool");
+		welcomeTitle.setBounds(5, 5, 784, 27);
+		welcomeTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(welcomeTitle);
+		welcomeTitle.setFont(new Font("Tahoma", Font.PLAIN, 22));
 
 		JPanel boardGrid = new JPanel();
 		boardGrid.setBounds(new Rectangle(5, 32, 400, 400));
@@ -99,29 +100,85 @@ public class MainFrame extends JFrame {
 		currentQueens.setText("0");
 		countPanel.add(currentQueens);
 		currentQueens.setColumns(2);
+
+		JTextArea instructionTextArea = new JTextArea();
+		instructionTextArea.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		instructionTextArea.setText("Welcome to the N-Queens Problem tool.\r\n- Place queens by clicking on squares, get rid \r\n   of them by clicking again\r\n- Queens cannot be in the same row, column,\r\n   or diagonal.\r\n- Check your answer by clicking the check \r\n   solution button");
+		instructionTextArea.setEditable(false);
+		instructionTextArea.setBounds(425, 40, 350, 150);
+		contentPane.add(instructionTextArea);
+
+		JTextArea checkerOutput = new JTextArea();
+		checkerOutput.setBounds(425, 200, 350, 150);
+		contentPane.add(checkerOutput);
 		
 		JButton checkSolution = new JButton("Check Solution");
-		checkSolution.setBounds(534, 372, 138, 60);
+		checkSolution.setBounds(524, 363, 140, 60);
 		contentPane.add(checkSolution);
 		
-		JTextArea instructionTextArea = new JTextArea();
-		instructionTextArea.setEditable(false);
-		instructionTextArea.setBounds(425, 40, 300, 300);
-		contentPane.add(instructionTextArea);
+		ImageIcon queenIcon = new ImageIcon(MainFrame.class.getResource("Freddie-Mercury.jpg"));
+		ImageIcon badQueenIcon = new ImageIcon(MainFrame.class.getResource("Freddie-Mercury-Angry.jpg"));
+
+		ActionListener checkListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+
+				ArrayList<Integer> rowList = new ArrayList<Integer>();
+				ArrayList<Integer> colList = new ArrayList<Integer>();
+
+				boolean rowCheck = true;
+				boolean colCheck = true;
+				boolean diaCheck = true;
+
+				for (int row = 0; row < gridArray.length; row++) {
+					for (int col = 0; col < gridArray[row].length; col++) {
+						if(gridArray[row][col].isSelected()){
+							rowList.add(row);
+							colList.add(col);
+							//Resets the icon in case it was marked from from a
+							//previous check.
+							gridArray[row][col].setIcon(queenIcon);
+						}
+					}
+				}
+				System.out.println(rowList);
+				System.out.println(colList);
+
+				checkerOutput.setText("");
+				for(int i = 0; i < rowList.size(); i++){
+					for(int j = 0; j < rowList.size(); j++){
+						if(rowList.get(i).compareTo(rowList.get(j)) == 0 && (i != j)){
+							rowCheck = false;
+							checkerOutput.append("(" + rowList.get(i) + "," + colList.get(i) + ") conflicts with ("
+									+ rowList.get(j) + "," + colList.get(j) + ")\n");
+							gridArray[rowList.get(i)][colList.get(j)].setIcon(badQueenIcon);
+						}
+						if(colList.get(i).compareTo(colList.get(j)) == 0 && (i != j)){
+							colCheck = false;
+							checkerOutput.append("(" + rowList.get(i) + "," + colList.get(i) + ") conflicts with ("
+									+ rowList.get(j) + "," + colList.get(j) + ")\n");
+							gridArray[rowList.get(i)][colList.get(j)].setIcon(badQueenIcon);
+						}
+					}
+				}
+			}
+		};
+		checkSolution.addActionListener(checkListener);
+
+
+
 
 		gridArray = new JToggleButton[8][8];
 
-		ImageIcon queenIcon = new ImageIcon(MainFrame.class.getResource("Freddie-Mercury.jpg"));
-
-		ActionListener buttonListener = new ActionListener() {
+		ActionListener boardListener = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
 				//Creates a temp button that represents the clicked one.
 				JToggleButton selectedButton = (JToggleButton) event.getSource();
-				
+
 				for (int row = 0; row < gridArray.length; row++) {
 					for (int col = 0; col < gridArray[row].length; col++) {
-						// Just helpful console output---User won't see it
+						//Reference console output, user won't see this
 						if (gridArray[row][col] == selectedButton) {
 							if (selectedButton.isSelected()) {
 								selectedButton.setIcon(queenIcon);
@@ -139,8 +196,8 @@ public class MainFrame extends JFrame {
 				}
 			}
 		};
-		
 
+		//Populate the chess board with toggle buttons
 		for (int row = 0; row < gridArray.length; row++) {
 			for (int col = 0; col < gridArray[row].length; col++) {
 				if ((col % 2 == 1 && row % 2 == 1) || (col % 2 == 0 && row % 2 == 0)) {
@@ -150,7 +207,7 @@ public class MainFrame extends JFrame {
 					gridArray[row][col] = new JToggleButton();
 					gridArray[row][col].setBackground(Color.WHITE);
 				}
-				gridArray[row][col].addActionListener(buttonListener);
+				gridArray[row][col].addActionListener(boardListener);
 				boardGrid.add(gridArray[row][col]);
 			}
 		}
